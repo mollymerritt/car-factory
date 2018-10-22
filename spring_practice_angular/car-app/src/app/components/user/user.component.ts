b73cbad5e10d608bfd9282524c7fe8b465c5499d
mollymerritt <merritt.mry@gmail.com>
2018-10-22 10:38:00 -0500

    updated user component

--- spring_practice_angular/car-app/src/app/components/user/user.component.ts
+++ spring_practice_angular/car-app/src/app/components/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { NavbarService } from 'src/app/services/navbar.service';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  private user: User;
+
+  constructor(
+    private navbarService: NavbarService,
+    private router: Router,
+    private cookieService: CookieService) { }
 
   ngOnInit() {
+    if (!this.navbarService.isLoggedIn()) {
+      this.router.navigate(['/home']);
+    }
+    this.user = JSON.parse(this.cookieService.get('user'));
   }
 
 }
