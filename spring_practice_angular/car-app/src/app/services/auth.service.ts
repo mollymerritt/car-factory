35e3a37c6ff415c3d89c26d91053ce6ebcb2c9a8
mollymerritt <merritt.mry@gmail.com>
2018-10-21 23:07:20 -0500

    added register method in login service

--- spring_practice_angular/car-app/src/app/services/auth.service.ts
+++ spring_practice_angular/car-app/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
+import { User } from 'src/app/models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,11 @@ export class AuthService {
   constructor(private http: HttpClient, private cookieService: CookieService) { }
 
   login(username: string, password: string): Observable<any> {
-    const u = { username: username, password: password };
-    return this.http.post<any>('http://localhost:5555/login', u);
+    const user = { username: username, password: password };
+    return this.http.post<any>('http://localhost:5555/login', user);
   }
 
-  
+  register(user: User): Observable<any> {
+    return this.http.post<any>('http://localhost:5555/users', user);
+  }
 }
